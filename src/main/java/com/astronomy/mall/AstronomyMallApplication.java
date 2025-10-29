0875df4472e0fa1f510978629922aaba61639d9d
吴梓骞 <wuziqian@bosssoft.com.cn>
2025-10-29 17:07:39 +0800

    1

---
+++ src/main/java/com/astronomy/mall/AstronomyMallApplication.java
@@ -0,0 +1,13 @@
+package com.astronomy.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstronomyMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstronomyMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astronomy/mall/AstronomyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astronomy.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstronomyMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
